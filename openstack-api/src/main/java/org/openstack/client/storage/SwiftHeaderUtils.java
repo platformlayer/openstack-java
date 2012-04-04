55c8a145bf02d0cd40f1dcea6c17e5ca67de9cd2
Justin Santa Barbara <justin@fathomdb.com>
2012-04-04 00:22:09 -0700

    Fix Jenkins classpath problems

--- openstack-api/src/main/java/org/openstack/client/storage/SwiftHeaderUtils.java
+++ openstack-api/src/main/java/org/openstack/client/storage/SwiftHeaderUtils.java
@@ -3,7 +3,7 @@ package org.openstack.client.storage;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Logger;
 
 import org.openstack.client.common.HeadResponse;
@@ -13,13 +13,20 @@ import org.openstack.client.internals.SimpleClassInfo.FieldInfo;
 import org.openstack.model.storage.ContainerProperties;
 import org.openstack.model.storage.ObjectProperties;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.common.base.Function;
+import com.google.common.collect.MapMaker;
 
 class SwiftHeaderUtils {
 	static final Logger log = Logger.getLogger(SwiftHeaderUtils.class.getName());
 
+	// Guava has an issue with FinalizableReferenceQueue keeping ClassLoaders around...
+	// http://code.google.com/p/guava-libraries/issues/detail?id=92
+	// That has taken 4 years to fix (sigh)
+	// Which means that someone shipped a patched version called sisu-guava,
+	//  but they didn't change the namespace (sigh)
+	// And the signature changed so we get method-not-found errors at runtime (sigh)
+	// So we have to use the obsolete cache (sigh)
+	/*
 	static final LoadingCache<Class<?>, SimpleClassInfo> CLASS_INFO = CacheBuilder.newBuilder().build(
 			new CacheLoader<Class<?>, SimpleClassInfo>() {
 				@Override
@@ -27,7 +34,15 @@ class SwiftHeaderUtils {
 					return new SimpleClassInfo(c);
 				}
 			});
-
+	*/
+	static final ConcurrentMap<Class<?>, SimpleClassInfo> CLASS_INFO = new MapMaker().makeComputingMap(new Function<Class<?>, SimpleClassInfo>() {
+		@Override
+		public SimpleClassInfo apply(Class<?> c) {
+			return new SimpleClassInfo(c);
+		}
+	});
+			
+	
 	public static void unmarshalHeaders(HeadResponse response, Object properties, Map<String, String> customProperties,
 			String keyPrefix) {
 		for (Entry<String, List<String>> entry : response.getHeaders().entrySet()) {
@@ -62,7 +77,8 @@ class SwiftHeaderUtils {
 	private static SimpleClassInfo getClassInfo(Class<? extends Object> c) {
 		try {
 			return CLASS_INFO.get(c);
-		} catch (ExecutionException e) {
+			// TODO: Switch to ExecutionException when LoadingCache gets figured out
+		} catch (Throwable e) {
 			throw new IllegalStateException("Error reflecting on class info", e);
 		}
 	}
